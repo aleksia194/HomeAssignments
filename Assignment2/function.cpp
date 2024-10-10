bd35f34b839b2b85747dc16cf8dd9ed346eccbfe
Babikov Aleksandr <st135743@student.spbu.ru>
2024-10-11 02:41:44 +0300

    implementation of function code

--- Assignment2/function.cpp
+++ Assignment2/function.cpp
@@ -4,17 +4,28 @@
 #include "myheader.h"
 
 size_t getFileSize(const std::string& filename) {
-
+    return std::filesystem::file_size(filename);
 }
 
 void readFile(const std::string& filename, char* data, size_t size) {
-    
+    std::ifstream input_file(filename, std::ios::binary);
+    if (input_file) {
+        input_file.read(data, size);
+        input_file.close();
+    }
 }
 
 void reverseData(char* data, size_t size) {
-    
+    for (size_t i = 0; i < size / 2; ++i) {
+        std::swap(data[i], data[size - i - 1]);
+    }
 }
 
 void writeFile(const std::string& filename, const char* data, size_t size) {
-   
+    std::ofstream output_file(filename, std::ios::binary);
+    if (output_file) {
+        output_file.write(data, size);
+        output_file.close();
+    }
 }
+

328da9b8c619ec48854f3835950be2ebe350c11d
Babikov Aleksandr <st135743@student.spbu.ru>
2024-10-11 02:25:11 +0300

    Adding core c++ files and defining the required functions

---
+++ Assignment2/function.cpp
@@ -0,0 +1,20 @@
+/*     TASK_2 Babikov Aleksandr st135743@student.spbu.ru     */
+
+
+#include "myheader.h"
+
+size_t getFileSize(const std::string& filename) {
+
+}
+
+void readFile(const std::string& filename, char* data, size_t size) {
+    
+}
+
+void reverseData(char* data, size_t size) {
+    
+}
+
+void writeFile(const std::string& filename, const char* data, size_t size) {
+   
+}
---
+++ Assignment2/main.cpp
@@ -0,0 +1,16 @@
+/*     TASK_2 Babikov Aleksandr st135743@student.spbu.ru     */
+
+
+#include <iostream>
+#include "myheader.h"
+
+int main() {
+    std::string inputFilename = "text.txt";
+    std::string outputFilename = "temptext.txt";
+    
+
+
+    delete[] data;
+    return 0;
+}
+

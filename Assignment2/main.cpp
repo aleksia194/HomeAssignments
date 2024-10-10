dcade7caa3a45ed59df14819253ad7755e2fe220
Babikov Aleksandr <st135743@student.spbu.ru>
2024-10-11 02:34:28 +0300

    definition structure main.cpp

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -8,7 +8,18 @@ int main() {
     std::string inputFilename = "text.txt";
     std::string outputFilename = "temptext.txt";
     
+    size_t fileSize = getFileSize(inputFilename);
+    char* data = new char[fileSize];
 
+    std::cout << fileSize << "b file size" << std::endl;
+
+    readFile(inputFilename, data, fileSize);
+    reverseData(data, fileSize);
+
+    fileSize = getFileSize(inputFilename);
+    std::cout << fileSize << "b file size" << std::endl;
+
+    writeFile(outputFilename, data, fileSize);
 
     delete[] data;
     return 0;

c8f2eef0d750620ffcf29a4f44ce629e848af728
Babikov Aleksandr <st135743@student.spbu.ru>
2024-10-11 12:30:33 +0300

    implementation of array functions

--- Assignment2b/function.cpp
+++ Assignment2b/function.cpp
@@ -11,26 +11,34 @@ Stack::Stack(int size) : capacity(size), topIndex(-1) {
 }
 
 Stack::~Stack() {
-
+    delete[] stackArray;
 }
 
 void Stack::push(double value) {
-    
+    if (topIndex >= capacity - 1) {
+        throw std::overflow_error("Стек переполнен");
+    }
+    stackArray[++topIndex] = value;
 }
 
 double Stack::pop() {
-   
+    if (topIndex < 0) {
+        throw std::underflow_error("Стек пустой");
+    }
+    return stackArray[topIndex--];
 }
 
 double Stack::top() const {
-    
+    if (topIndex < 0) {
+        throw std::underflow_error("Стек пустой");
+    }
+    return stackArray[topIndex];
 }
 
 bool Stack::isEmpty() const {
-
+    return topIndex == -1;
 }
 
 double evaluateRPN(const std::string& expression) {
     
 }
-

33808e66aa76c200b3005025961b70ef0a30b61a
Babikov Aleksandr <st135743@student.spbu.ru>
2024-10-11 12:24:28 +0300

    adding headings and outlining the required class features

--- Assignment2b/function.cpp
+++ Assignment2b/function.cpp
@@ -2,4 +2,35 @@
 
 
 #include "myheader.h"
+#include <stdexcept>
+#include <sstream>
+#include <cctype>
+
+Stack::Stack(int size) : capacity(size), topIndex(-1) {
+    stackArray = new double[capacity];
+}
+
+Stack::~Stack() {
+
+}
+
+void Stack::push(double value) {
+    
+}
+
+double Stack::pop() {
+   
+}
+
+double Stack::top() const {
+    
+}
+
+bool Stack::isEmpty() const {
+
+}
+
+double evaluateRPN(const std::string& expression) {
+    
+}
 
--- Assignment2b/myheader.h
+++ Assignment2b/myheader.h
@@ -4,6 +4,25 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
 
+#include <string>
+
+class Stack {
+private:
+    double* stackArray;
+    int topIndex;
+    int capacity;
+
+public:
+    Stack(int size);
+    ~Stack();
+    void push(double value);
+    double pop();
+    double top() const;
+    bool isEmpty() const;
+};
+
+double evaluateRPN(const std::string& expression);
+
 
 #endif // MYHEADER_H
 

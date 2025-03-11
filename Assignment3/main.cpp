d82f3925638ff4b4c081db3e4b769c0d210f3b8d
Александр Бабиков <st135743@student.spbu.ru>
2025-03-11 21:35:04 +0300

    litl chenge ang test performance

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -1,3 +1,19 @@
 /*     Assignment3 Babikov Aleksandr st135743@student.spbu.ru     */
 
 
+#include "Transformer.h"
+#include "Weapon.h"
+
+int main() {
+    Transformer optimus;
+    Weapon blaster("Blaster", 50);
+    Weapon sword("Energy Sword", 80);
+
+    optimus.equipWeapon(&blaster);
+    optimus.attack();
+
+    optimus.equipWeapon(&sword);
+    optimus.attack();
+
+    return 0;
+}
